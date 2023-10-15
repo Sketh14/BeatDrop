be74121b0a20834fd28730920b4f4ed91924887a
Sketh <www.sketh007@gmail.com>
2023-10-15 19:02:16 +0530

    Adding Background to Main Gameplay
    Adding parallax to the clous behind

--- Scripts/MainMenuController.gd
+++ Scripts/MainMenuController.gd
@@ -1,8 +1,38 @@
-extends CanvasLayer
+extends Node2D
 
+var startDropPos
+var endDropPos
+var startBucketPos
+var endBucketPos
+var time_taken = 0.0
+@export var drop : Control
+@export var bucket : Control
+@export var speedMult : float = 1.0
 
 func _on_start_bt_pressed():
 	get_tree().change_scene_to_file("res://Scenes/MainGame.tscn")
 
 func _on_exit_bt_pressed():
 	get_tree().quit()
+	
+func _process(delta):
+	time_taken += delta * speedMult
+
+	if (time_taken > 1):
+		time_taken = 0
+		var tempPos = startBucketPos
+		startBucketPos = endBucketPos
+		endBucketPos = tempPos
+
+	#drop.set_global_position(lerp(start_pos, end_pos, time_taken))
+	#print($"../Control/BG_Drop".position)
+	drop.position = Vector2(lerp(startDropPos, endDropPos, time_taken))
+	bucket.position = Vector2(lerp(startBucketPos, endBucketPos, time_taken))
+
+func _ready():
+	startDropPos = Vector2(205,262)
+	endDropPos = Vector2(238,703)
+	startBucketPos = Vector2(66,715)
+	endBucketPos = Vector2(120,684)
+	#drop = $"../Control/BG_Drop" as Control
+	#print($"../Control/BG_Drop".position)
